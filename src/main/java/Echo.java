6f4daa4ff9650fdd435acce547c0cf758444e56e
Nabijon Nazarov <barahush@gmail.com>
2026-08-27 18:22:51 +0800

    Level-1: Updated the code to echo commands entered by the user, and exit when the user types the command bye. Likewise, I created two private methods: printGreeting and printMessage for a cleaner code, and added Javadocs for them.

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Echo {
     private static final String LINE = "____________________________________________________________";
     private static final String BANNER = " _____ ____ _   _  ___  \n"
@@ -8,13 +10,39 @@ public class Echo {
     private static final String NAME = "Echo";
 
     public static void main(String[] args) {
+        printGreeting();
+        Scanner scanner = new Scanner(System.in);
+
+        while(scanner.hasNextLine()) {
+            String userInput = scanner.nextLine();
+            if(userInput.equals("bye")) {
+                break;
+            }
+            printMessage(userInput);
+        }
+        printMessage("Bye. Hope to see you again soon!");
+    }
+
+    /**
+     * Prints a Greeting message with ECHO banner
+     */
+    private static void printGreeting() {
         System.out.println(LINE);
         System.out.println(BANNER);
         System.out.println("Hello! I'm " + NAME + ".");
         System.out.println("What can I do for you?");
         System.out.println(LINE);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(LINE);
-
     }
+
+    /**
+     * Prints message in between two lines
+     *
+     * @param message message to display
+     */
+    private static void printMessage(String message) {
+        System.out.println(LINE);
+        System.out.println(message);
+        System.out.println(LINE);
+    }
+
 }
